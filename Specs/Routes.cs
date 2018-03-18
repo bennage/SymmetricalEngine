af6fb49039a038f842d037f0aabce06b5ea30e07
Christopher Bennage <christopher@bennage.com>
2018-03-17 22:29:20 -0700

    2nd test

--- Specs/Routes.cs
+++ Specs/Routes.cs
@@ -6,18 +6,30 @@ namespace Specs
 {
     public class When_calculating_routes_in_space
     {
-        [Fact]
-        public void x()
+        Space space;
+
+        public When_calculating_routes_in_space()
         {
-            var space = new Space();
+            space = new Space();
             space.AddSector(1, new[] { 2 });
             space.AddSector(2, new[] { 1, 3 });
             space.AddSector(3, new[] { 2, 4 });
             space.AddSector(4, new[] { 1, 5 });
             space.AddSector(5, new[] { 2 });
+        }
 
+        [Fact]
+        public void from_1_to_2()
+        {
             var r = Movement.Route(1, 2, space).ToArray();
             Assert.Equal(r[0], 2);
+        }
+
+        public void from_1_to_3()
+        {
+            var r = Movement.Route(1, 3, space).ToArray();
+            Assert.Equal(r[0], 2);
+            Assert.Equal(r[1], 3);
         }
     }
 
